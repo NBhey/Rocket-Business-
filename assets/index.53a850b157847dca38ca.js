2417712f60c3eaf91168ae7762956050cde55ae9
NBhey <NBhey@users.noreply.github.com>
2024-05-24 13:14:17 +0000

    Deploying to gh-pages from @ NBhey/Rocket-Business-@01cc84d08e90bcdc2234412baeba57fdbb9c7603 🚀

--- assets/index.53a850b157847dca38ca.js
+++
@@ -1,1 +0,0 @@
-import"./style/style.css";import{dropList}from"./js/dropList.js";console.log("Я подключен");const menuListBtn=document.querySelector(".header button");console.log(menuListBtn),menuListBtn.addEventListener("touchend",dropList);
