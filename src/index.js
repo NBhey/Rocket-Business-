99db07080a9e3a917f4584224ac3101f913c2b29
Oganes <og.marckaryan@yandex.ru>
2024-05-28 21:28:20 +0300

    add js, php and desktop layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import "./style/style.css"
-import { dropList } from "./js/dropList.js"
+import { dropList, dropForm, dropDesktopForm } from "./js/drop.js"
 import { showSlide,nextSlide,prevSlide } from "./js/slider.js";
-console.log('Я подключен')
-
-const menuListBtn = document.querySelector('#drop-menu')
-menuListBtn.addEventListener('click' , dropList );
-
+import { submitForm } from "./js/sendForm.js";
 showSlide()
 
 const nextSlideBtn = document.querySelector('.services__arrow button:last-child')
@@ -14,3 +10,23 @@ nextSlideBtn.addEventListener('click',nextSlide )
 
 const prevSlideSlideBtn = document.querySelector('.services__arrow button:first-child')
 prevSlideSlideBtn.addEventListener('click',prevSlide )
+
+
+const menuListBtn = document.querySelector('#drop-menu')
+menuListBtn.addEventListener('click' , dropList );
+
+const formDesktop = document.getElementById('book')
+formDesktop.addEventListener('click', dropDesktopForm)
+
+const form = document.querySelector('.drop-menu__feedback')
+form.addEventListener('click', dropForm)
+
+const sendForm = document.querySelector('.form-feedback')
+sendForm.addEventListener('submit', submitForm)
+
+const sendFormDesktop = document.querySelector('.contact-desktop__feedback_hidden')
+sendFormDesktop.addEventListener('submit', submitForm)
+
+
+
+
--- src/js/dropList.js
+++
@@ -1,6 +0,0 @@
-
-export function dropList(){
-    document.querySelector(".header__drop-menuBtn").classList.toggle("header__drop-menuBtnShow");
-    document.querySelector(".header__drop-menu").classList.toggle("header__drop-menuShow");
-}
-
